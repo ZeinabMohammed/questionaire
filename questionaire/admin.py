caae1cf2f4010637437fc0046b7bf649b1bce994
zeinabmohammed <zobalhlooba@gmail.com>
2019-08-06 13:26:57 +0200

    create questionaire database

--- questionaire/admin.py
+++ questionaire/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(MultipleChoices)
+admin.site.register(TextAnswer)
+admin.site.register(File)
+admin.site.register(CheckBox)
--- questionaire/models.py
+++ questionaire/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+	Question_text = models.CharField(max_length=120)
+	user          = models.ForeignKey(User, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.Question_text
+
+class MultipleChoices(models.Model):
+	question    = models.ForeignKey(Question,on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes       = models.IntegerField(default=0)
+	def __str__(self):
+		return self.choice_text
+
+class TextAnswer(models.Model):
+	question    = models.ForeignKey(Question,on_delete=models.CASCADE)
+	answer      = models.CharField(max_length=200)
+	def __str__(self):
+		return self.answer
+class File(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    file     = models.FileField(upload_to='static_cdn')
+    def __str__(self):
+    	return self.file.name
+class CheckBox(models.Model):
+	question = models.ForeignKey(Question,on_delete=models.CASCADE)
+	checkbox = models.CharField(max_length=120)
+	def __str__(self):
+		return self.checkbox
